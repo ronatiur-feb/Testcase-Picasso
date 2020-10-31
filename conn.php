c20133a7ed9d119f3fb1628eb2ba4092065b6fb8
ronatiur-feb <ronatiurfebriani09@gmail.com>
2020-10-31 15:10:11 +0700

    test

--- conn.php
+++
@@ -1,9 +0,0 @@
-	<?php
-	global $conn;
-	$conn = mysqli_connect("localhost","root","","picasso");
-	    
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-
-	?>
